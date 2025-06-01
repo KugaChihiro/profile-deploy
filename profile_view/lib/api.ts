96c15d31173838c8644dc5cb30b7daf96a923006
KugaChihiro <c.kuga.24.4.1410@4nonome.com>
2025-06-01 11:53:29 +0900

    デプロイのための修正

--- profile_view/lib/api.ts
+++ profile_view/lib/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  // baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "https://ca-profile-01.graydune-73910fd2.japaneast.azurecontainerapps.io"
 });
+
 
 export default api;
